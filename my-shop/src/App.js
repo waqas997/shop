6967bf891a530a7b42319096a18c2b789c851d61
waqas997 <waqasahmed2k15@gmail.com>
2024-03-11 17:58:38 +0500

    aa

---
+++ my-shop/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>how are you</>
+  );
+}
+
+export default App;
---
+++ my-shop/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div>
+            
+        </div>
+      </nav>
+    </div>
+  );
+}
